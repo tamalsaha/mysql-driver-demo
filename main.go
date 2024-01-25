0556b16bf4f55daf79bc678fcd91f01a7126fa64
Tamal Saha <tamal@appscode.com>
2024-01-24 21:14:59 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "database/sql"
+import (
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+)
 import _ "github.com/go-sql-driver/mysql"
 
 func main() {
-	db, err := sql.Open("mysql", "user:password@/dbname")
+	cfg, err := mysql.ParseDSN("tcp(mysql-demo.mysql.svc:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	cfg.User = "user"
+	cfg.Passwd = "password"
+	// cfg.DBName = "adb"
+	fmt.Println(cfg.FormatDSN())
+	//
+	//_, err := sql.Open("mysql", "tcp(mysql-demo.mysql.svc:3306)/")
+	//if err != nil {
+	//	panic(err)
+	//}
 }
